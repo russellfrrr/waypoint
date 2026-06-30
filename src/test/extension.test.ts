cd2ee280e1a1d27251ac0d859b057f107417dc11
Russell Ferrero <russellferrero00@gmail.com>
2026-06-30 18:01:16 +0800

    test: cover file reference resolution

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,52 @@
 import * as assert from 'assert';
+import * as fs from 'fs';
+import * as os from 'os';
+import * as path from 'path';
+import { resolveFileReference } from '../utils/pathUtils';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+suite('Path Utilities', () => {
+	let tempDirectory: string;
+	let currentFilePath: string;
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+	setup(() => {
+		tempDirectory = fs.mkdtempSync(path.join(os.tmpdir(), 'waypoint-'));
+		currentFilePath = path.join(tempDirectory, 'src', 'current.ts');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+		fs.mkdirSync(path.dirname(currentFilePath), { recursive: true });
+		fs.writeFileSync(currentFilePath, '');
+	});
+
+	teardown(() => {
+		fs.rmSync(tempDirectory, { recursive: true, force: true });
+	});
+
+	test('resolves typescript files without extensions', () => {
+		const targetPath = path.join(tempDirectory, 'src', 'parser.ts');
+
+		fs.writeFileSync(targetPath, '');
+
+		assert.strictEqual(resolveFileReference(currentFilePath, './parser'), targetPath);
+	});
+
+	test('resolves folder index files', () => {
+		const targetDirectory = path.join(tempDirectory, 'src', 'components');
+		const targetPath = path.join(targetDirectory, 'index.ts');
+
+		fs.mkdirSync(targetDirectory, { recursive: true });
+		fs.writeFileSync(targetPath, '');
+
+		assert.strictEqual(resolveFileReference(currentFilePath, './components'), targetPath);
+	});
+
+	test('resolves files with explicit extensions', () => {
+		const targetPath = path.join(tempDirectory, 'src', 'config.json');
+
+		fs.writeFileSync(targetPath, '{}');
+
+		assert.strictEqual(resolveFileReference(currentFilePath, './config.json'), targetPath);
+	});
+
+	test('returns undefined when a file reference cannot be resolved', () => {
+		assert.strictEqual(resolveFileReference(currentFilePath, './missing'), undefined);
 	});
 });
