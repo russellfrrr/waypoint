839ab85da95ae1ad6cf29a4f7e11d50e14de5ea6
Russell Ferrero <russellferrero00@gmail.com>
2026-07-01 01:31:47 +0800

    test: cover import and export reference detection

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -2,7 +2,9 @@ import * as assert from 'assert';
 import * as fs from 'fs';
 import * as os from 'os';
 import * as path from 'path';
+import * as vscode from 'vscode';
 import { resolveFileReference } from '../utils/pathUtils';
+import { isImportOrExportReference } from '../utils/referenceUtils';
 
 suite('Path Utilities', () => {
 	let tempDirectory: string;
@@ -48,5 +50,56 @@ suite('Path Utilities', () => {
 
 	test('returns undefined when a file reference cannot be resolved', () => {
 		assert.strictEqual(resolveFileReference(currentFilePath, './missing'), undefined);
+	});
+});
+
+suite('Reference Utilities', () => {
+	const createDocument = async (content: string): Promise<vscode.TextDocument> => {
+		return vscode.workspace.openTextDocument({
+			content,
+			language: 'typescript',
+		});
+	};
+
+	const getRangeForText = (
+		document: vscode.TextDocument,
+		text: string
+	): vscode.Range => {
+		const startOffset = document.getText().indexOf(text);
+
+		assert.notStrictEqual(startOffset, -1);
+
+		const start = document.positionAt(startOffset);
+		const end = document.positionAt(startOffset + text.length);
+
+		return new vscode.Range(start, end);
+	};
+
+	test('detects import declarations', async () => {
+		const document = await createDocument("import { Parser } from './parser';");
+		const range = getRangeForText(document, "'./parser'");
+
+		assert.strictEqual(isImportOrExportReference(document, range), true);
+	});
+
+	test('detects named export declarations', async () => {
+		const document = await createDocument("export { Parser } from './parser';");
+		const range = getRangeForText(document, "'./parser'");
+
+		assert.strictEqual(isImportOrExportReference(document, range), true);
+	});
+
+	test('detects export-all declarations', async () => {
+		const document = await createDocument("export * from './parser';");
+		const range = getRangeForText(document, "'./parser'");
+
+		assert.strictEqual(isImportOrExportReference(document, range), true);
+	});
+
+	test('ignores random relative strings', async () => {
+		const document = await createDocument("const randomPath = './parser';");
+		const range = getRangeForText(document, "'./parser'");
+
+		assert.strictEqual(isImportOrExportReference(document, range), false);
 	});
 });
